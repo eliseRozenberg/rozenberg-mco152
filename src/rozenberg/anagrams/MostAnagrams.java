76aae68018ce8dd2163274ecf285bee28f5e2b18
elise rozenberg <eliserozenberg@gmail.com>
2015-11-08 05:54:56 -0500

    update files

--- src/rozenberg/anagrams/MostAnagrams.java
+++ src/rozenberg/anagrams/MostAnagrams.java
@@ -3,8 +3,11 @@ package rozenberg.anagrams;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MostAnagrams {
@@ -39,18 +42,26 @@ public class MostAnagrams {
 
 		int largest = 0;
 		line = null;
+		ArrayList<String> largestList = new ArrayList<String>();
 		// run through sorted for most common anagram
 		for (Map.Entry<String, Integer> entry : mapSorted.entrySet()) {
 			if (entry.getValue() > largest) {
+				largestList.clear();
+				largestList.add(entry.getKey());
 				largest = entry.getValue();
-				line = entry.getKey();
+			} else if (entry.getValue() == largest) {
+				largestList.add(entry.getKey());
 			}
 		}
+
 		// run through to find words of most common anagram
-		for (Map.Entry<String, String> entry : map.entrySet()) {
-			if (entry.getValue().equals(line)) {
-				System.out.print(entry.getKey() + " ");
+		for (int i = 0; i < largestList.size(); i++) {
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+				if (entry.getValue().equals(largestList.get(i))) {
+					System.out.print(entry.getKey() + " ");
+				}
 			}
+			System.out.println();
 		}
 
 	}// end main
--- src/rozenberg/scrabble/ScrabbleDictionaryTest.java
+++ src/rozenberg/scrabble/ScrabbleDictionaryTest.java
@@ -8,13 +8,17 @@ import org.junit.Test;
 public class ScrabbleDictionaryTest {
 
 	@Test
-	public void testContains() throws IOException {
+	public void testContainsTrue() throws IOException {
 		ScrabbleDictionary q = new ScrabbleDictionary();
 
 		boolean contains = q.contains("herringbone");
 		Assert.assertTrue(contains);
+	}
+	@Test
+	public void testContainsFalse() throws IOException {
+		ScrabbleDictionary q = new ScrabbleDictionary();
 
-		contains = q.contains("ambfg");
+		boolean contains = q.contains("amfg");
 		Assert.assertFalse(contains);
 	}
 }
